551fc3c5d6d5938b6bb8ee256a0e3a0290abd9cb
prem <praulayar994@gmail.com>
2024-02-03 13:08:23 +0530

    03-02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,166 @@
-import Image from "next/image";
+import Image from 'next/image'
+import { IoStarSharp } from 'react-icons/io5'
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
-export default function Home() {
+import bgPatternTopDesktop from '@/assests/images/bg-pattern-top-desktop.svg'
+import bgPatternBottomDesktop from '@/assests/images/bg-pattern-bottom-desktop.svg'
+
+import bgPatternTopMobile from '@/assests/images/bg-pattern-top-mobile.svg'
+import bgPatternBottomMobile from '@/assests/images/bg-pattern-bottom-mobile.svg'
+
+import user1 from '@/assests/images/image-colton.jpg'
+import user2 from '@/assests/images/image-irene.jpg'
+import user3 from '@/assests/images/image-anne.jpg'
+
+const userReviews: UserCardProps[] = [
+  {
+    name: 'Colton Smith',
+    avatar: user1,
+    review:
+      'We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!',
+  },
+  {
+    name: 'Irene Roberts',
+    avatar: user2,
+    review:
+      'Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.',
+    style: 'md:mt-5',
+  },
+  {
+    name: 'Anne Wallace',
+    avatar: user3,
+    review:
+      'Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!',
+    style: 'md:mt-10',
+  },
+]
+
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className='min-h-screen h-full w-full  flex justify-center items-center '>
+      <BGImges />
+      <main className='max-w-6xl w-full flex flex-col gap-7 '>
+        <section className='flex justify-between px-4 lg:px-10 py-5 flex-col md:flex-row  gap-5 md:gap-0'>
+          <div className='md:w-1/2 md:max-w-[400px] flex flex-col gap-5 '>
+            <h2 className='text-4xl lg:text-5xl text-very-dark-magenta font-bold leading-10 text-center md:text-start px-10 md:px-0'>
+              10,000+ of our users love our products.
+            </h2>
+            <p className='text-dark-grayish-magenta leading-5 text-sm lg:text-base text-center md:text-start px-5 md:px-0'>
+              We only provide great products combined with excellent customer
+              service. See what our satisfied customers are saying about our
+              services.
+            </p>
+          </div>
+
+          <div className='md:w-1/2 flex flex-col justify-center gap-5 sm:px-14 md:px-0 xl:pl-14  items-center'>
+            <Review count={5} label={'Reviews'} />
+            <Review
+              style='md:ml-4 lg:ml-5 xl:ml-10'
+              count={5}
+              label={'Report Guru'}
             />
-          </a>
+            <Review
+              style='md:ml-8 lg:ml-16 xl:ml-20'
+              count={5}
+              label={'Best Tech'}
+            />
+          </div>
+        </section>
+
+        <section className='flex justify-between gap-5 px-4 lg:px-10 py-5 flex-col sm:flex-row items-center sm:items-start'>
+          {userReviews.map((review) => (
+            <UserCard {...review} />
+          ))}
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
+
+type StarsProps = { count: number; label: string; style?: string }
+
+function Review({ count, label, style }: StarsProps) {
+  let stars: number[] = []
+  for (let i = 1; i <= count; i++) {
+    stars.push(i)
+  }
+
+  return (
+    <div
+      className={`flex items-center justify-center md:justify-start gap-2 md:gap-6 bg-light-grayish-magenta px-4 lg:px-7 py-3 rounded-xl md:rounded-md w-full max-w-[400px] flex-col md:flex-row ${style}`}
+    >
+      <div className='flex  gap-1.5 text-amber-400'>
+        {stars.map((s) => (
+          <IoStarSharp className='text-lg' />
+        ))}
+      </div>
+      <p className='text-very-dark-magenta font-semibold text-xs lg:text-sm'>
+        Rated {count} Stars in {label}
+      </p>
+    </div>
+  )
+}
+
+type UserCardProps = {
+  name: string
+  avatar: string | StaticImport
+  review: string
+  style?: string
+}
+
+function UserCard({ name, avatar, review, style }: UserCardProps) {
+  return (
+    <div
+      className={`bg-very-dark-magenta rounded-lg flex flex-col gap-5 px-6 lg:px-9 py-7 max-w-[400px] h-fit ${style}`}
+    >
+      <div className='flex gap-4'>
+        <div>
+          <Image className='rounded-full h-8 w-8 md:h-10 md:w-10' src={avatar} alt='avatar' />
+        </div>
+        <div>
+          <h4 className='text-white font-medium text-xs md:text-base'>
+            {name}
+          </h4>
+          <p className='text-soft-pink text-xs md:text-base'>Verified Buyer</p>
         </div>
       </div>
+      <div>
+        <p className='text-white text-sm lg:text-base'>"{review}"</p>
+      </div>
+    </div>
+  )
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+function BGImges() {
+  return (
+    <div className=''>
+      <div className=' hidden md:block'>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          className='absolute top-0 left-0'
+          src={bgPatternTopDesktop}
+          alt='bg-pattern-top-desktopbg-pattern-top-desktop'
+        />
+        <Image
+          className='absolute bottom-0 right-0'
+          src={bgPatternBottomDesktop}
+          alt='bg-pattern-top-desktopbg-pattern-top-desktop'
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* mobiles images */}
+      <div className='md:hidden'>
+        <Image
+          className='absolute top-0 left-0'
+          src={bgPatternTopMobile}
+          alt='bg-pattern-top-desktopbg-pattern-top-desktop'
+        />
+        <Image
+          className='absolute bottom-0 right-0'
+          src={bgPatternBottomMobile}
+          alt='bg-pattern-top-desktopbg-pattern-top-desktop'
+        />
       </div>
-    </main>
-  );
+    </div>
+  )
 }
